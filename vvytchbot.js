34298c3cd0a832c3ba11e594684e9cdf0f368028
unknown <covertwallace@gmail.com>
2017-11-16 00:05:12 -0500

    setup vvychbot

---
+++ vvytchbot.js
@@ -0,0 +1,10 @@
+//define dependencies
+const twit = require('twit');
+
+const config = {
+	consumer_key: process.env.consumer_key,
+	consumer_secret: process.env.consumer_secret,
+	access_token: process.env.access_token,
+	access_token_secret: process.env.access_token_secret
+}
+const Twitter = new twit(config);
